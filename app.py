26f35906a01814ac8c52001c86ab6b2d3f160c3b
sharmavivek223 <sharma.vivek2231999@gmail.com>
2019-09-01 13:32:39 +0530

    jquery and json request added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request,jsonify
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import pickle
@@ -14,9 +14,18 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    prediction=-1
-    output=""
-    return render_template('index.html',prediction=prediction,output=output)
+    return render_template('index.html')
+
+@app.route('/background_process')
+def background_process():
+	try:
+		lang = request.args.get('review',type=str)
+		if lang.lower() == 'pos':
+			return jsonify({"result": "Positive","toggle":1})
+		elif lang.lower() == 'neg':
+			return jsonify({"result": "Negative","toggle":0})
+	except Exception as e:
+		return str(e)
 
 
 if __name__ == '__main__':
